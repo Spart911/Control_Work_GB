347c57b5d86f5439a0c615a73bd5e430901ec496
Egor Stolbovoy <egor200204222@gmail.com>
2023-12-10 15:44:51 +0300

    Инициализировать оставшиеся файлы

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] ArrFirst = new string[] { "one", "two", "tree", "four", "five", "six", "seven", "eight", "nine", "ten"};
+string[] ArrSecond = new string[ArrFirst.Length];
+void SecondArray(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if (arrayOne[i].Length <= 3)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(ArrFirst, ArrSecond);
+PrintArray(ArrSecond);
